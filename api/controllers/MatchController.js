df3d34c6389ae4613fe1b4fe3bfe58d2c2775e33
Patrick McDavid <pmcdavid@gmail.com>
2014-11-19 22:12:14 -0700

    new Quicksave method for matches

--- api/controllers/MatchController.js
+++ api/controllers/MatchController.js
@@ -3,9 +3,46 @@
  *
  * @description :: Server-side logic for managing matches
  * @help        :: See http://links.sailsjs.org/docs/controllers
+ * USAGE: http://localhost:1337/match/quicksave?p1=1&p2=2&p1score=3&p2score=6&p1team=yellow&p2team=black
  */
 
 module.exports = {
-	
+	quicksave: function(req, res) {
+        var p1Id = req.param('p1');
+        var p2Id = req.param('p2');
+
+        var p1Score = req.param('p1score');
+        var p2Score = req.param('p2score');
+
+        var p1Team = req.param('p1team');
+        var p2Team = req.param('p2team');
+
+
+        Match.create({}, function(err, match) {
+
+
+            MatchPlayer.create({
+                match: match.id,
+                player: p1Id,
+                score: p1Score,
+                team: p1Team
+            }, function(err, p1) {
+
+                MatchPlayer.create({
+                    match: match.id,
+                    player: p2Id,
+                    score: p2Score,
+                    team: p2Team
+                }, function(err, p2) {
+
+                    Match.findOne(match.id, function(err, match) {
+                       res.json(match);
+                    });
+
+                });
+            });
+
+        });
+    }
 };
 
